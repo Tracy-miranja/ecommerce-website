17ce7f678cdf9a0c4dcbe039e8b36cac702ac9f2
Tracy-miranja <barazatracy44@gmail.com>
2024-05-18 09:38:30 +0300

    Install react icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,24 @@
 // import React from "react";
 import logo from "../../assets/logoshop.png";
+import { IoSearchSharp } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="drop-shadow-md bg-white dark:bg-grey-900 dark:text-white duration-200 z-40">
       {/* upper Navbar */}
       <div className="bg-primary">
         <div className="container flex justify-between items-center">
           <div>
-            <a href="#" className="font-bold text-2xl sm:text-2xl flex gap-2">
+            <a
+              href="#"
+              className="font-bold text-2xl sm:text-2xl flex gap-2 text-white"
+            >
               <img
                 src={logo}
                 alt="NavBrand"
-                className="w-24 h-10 uppercase text-center"
+                className="w-24 h-10 uppercase text-center "
               />{" "}
-              Kushi-wear
+              <span className="text-red-500">Kushi</span>-wear
             </a>
           </div>
           {/* searchbar */}
@@ -24,6 +28,7 @@ const Navbar = () => {
               placeholder="search"
               className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-primary px-2 py-1 focus:outline-none focus:border-1 focus:border-orange-400"
             />
+            <IoSearchSharp />
           </div>
         </div>
       </div>
